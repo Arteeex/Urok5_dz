564150c023b91dc1d95f8a834a825e7ec578ad93
Arteeex <arteeex187@gmail.com>
2023-01-30 17:04:04 +0700

    dz_3_ready

---
+++ dz_3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите размерность массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+double [] array = new double [N];
+Random random = new Random();
+double Max = 1.0;
+
+Console.Write("Сгенерированный массив: ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.NextDouble() * 20;
+    Console.Write("{0:F2} ", array[i]);
+}
+Console.Write("\nМаксимальное: ");
+Console.WriteLine("{0:F2}", array.Max());
+Console.Write("\nМинимальное: ");
+Console.WriteLine("{0:F2}", array.Min());
+Console.Write("\nРазность: ");
+Console.WriteLine("{0:F2}", array.Max() - array.Min());
