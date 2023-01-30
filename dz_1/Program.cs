eb918eb81a113722a2db98515a3c4a3ea7564fef
Arteeex <arteeex187@gmail.com>
2023-01-30 17:00:15 +0700

    dz_1_ready

---
+++ dz_1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+Console.Write("Введите размерность массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] arr = new int [N];
+Random rnd = new Random();
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rnd.Next(100, 1000);
+}
+    foreach (var item in arr)
+    {
+     System.Console.Write(item + " ");
+    }
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if(arr[i] % 2 == 0)
+    {
+      count++;  
+    }
+    
+}
+System.Console.Write(" \nКоличество четных чисел в массиве: " +count);
